28516de1bffd2406df9c4a9656350c27735ece47
Roberto Giovanardi <roberto.giovanardi@bs-company.com>
2018-08-08 16:58:18 +0200

    added logging conf

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,19 +1,30 @@
 from flask import Flask, url_for, render_template, request, flash, redirect, session, abort
-import subprocess, os
+import subprocess, os, logging 
 from commands import commands
 
+import ipdb
+
+logging.basicConfig(
+    filename='server.log',
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s:%(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p'
+    )
 app = Flask(__name__)
 
 # index page: ask for first info
 @app.route('/', methods=['GET','POST'])
 def main():
     if not session.get('logged_in'):
+        logging.warn('User not logged in')
         return render_template('login.html')
     else:
         if request.method == 'GET':
+            logging.debug('get index page')
             return render_template('index.html')
         elif request.method == 'POST':
             install = request.form['choose1']
+#            logging.info('You choosed {}').format(install)
             return redirect(url_for('step2', install = install))
 
 # login step
@@ -21,8 +32,10 @@ def main():
 def do_admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
+        logging.info('Admin user login')
     else:
         flash('wrong password!')
+        logging.warning('Wrong password')
     return redirect(url_for('main'))
 #    return main()
 
@@ -31,22 +44,31 @@ def do_admin_login():
 def step2():
     if not session.get('logged_in'):
         return render_template('login.html')
+        logging.warn('User not logged in')
     else:
         if request.method == 'GET':
-            install = request.args.get('install')
+#            ipdb.set_trace()
+            install = request.args.get('choose1')
+            logging.info('You choosed {}'.format(install))
             return render_template('step2.html',install = install)
         elif request.method == 'POST':
             hostname = request.form['hostname']
             hostname2 = request.form['hostname2']
-            # now put some logic here
+            print(hostname)
+            print(hostname2)
             return redirect(url_for('step3',hostname = hostname))
 
 @app.route('/step3', methods=['GET', 'POST'])
 def step3():
     if not session.get('logged_in'):
         return render_template('login.html')
+        logging.warn('User not logged in')
     else:
         hostname = request.args.get('hostname')
+        hostname2 = request.args.get('hostname2')
+        logging.info('hostname 1: {}'.format(hostname))
+        if hostname2: 
+            logging.info('hostname 2: {}'.format(hostname2))
         bashCommand = commands['who']
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
